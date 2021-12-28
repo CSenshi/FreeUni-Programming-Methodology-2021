8e1f647b3910bd49c3426dbbaea251ff2dec8445
CSenshi <saba.pochkhua@gmail.com>
2021-12-28 14:44:07 +0400

    add s26

--- s26/Ex69.java
+++ s26/Ex69.java
@@ -1,5 +1,13 @@
 package s26;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 /**
@@ -12,5 +20,53 @@ import acm.program.GraphicsProgram;
  */
 public class Ex69 extends GraphicsProgram {
 
+	private JTextField messageInput;
+	private JButton addButton;
+	private int y;
+	private int X_OFF = 10;
+	private List<GLabel> messages;
 
+	@Override
+	public void init() {
+		// Initialize messages list
+		y = 0;
+		messages = new ArrayList<GLabel>();
+
+		// Initialize text field
+		messageInput = new JTextField(20);
+		add(messageInput, SOUTH);
+		messageInput.addActionListener(this);
+
+		// Initialize button
+		addButton = new JButton("Add");
+		add(addButton, SOUTH);
+
+		addActionListeners();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// 1. Get message
+		String message = messageInput.getText();
+
+		// 2. Add message on canvas
+		GLabel messageLabel = new GLabel(message);
+		add(messageLabel, X_OFF, y);
+
+		// 3. Increment message count
+		messages.add(messageLabel);
+
+		// 4. Check if last label is visible
+		double labelSize = messageLabel.getAscent();
+		if (messageLabel.getY() > getHeight()) {
+			// 4.1 if not move all labels up by one label's size
+			for (GLabel label : messages) {
+				label.move(0, -labelSize);
+			}
+			// 4.2 remove first label
+			messages.remove(0);
+		} else {
+			y += labelSize;
+		}
+	}
 }
--- s26/Ex70.java
+++ s26/Ex70.java
@@ -1,16 +1,64 @@
 package s26;
 
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 /**
- * თქვენი ამოცანაა გრაფიკული პროგრამის საშუალებით გააკეთოთ ჩატი.
+ * მთელ ფორმაზე ბადის დახატვა,
  * 
- * ფანჯარაზე, SOUTH რეგიონზე უნდა გქონდეთ ერთი ტექსტფილდი და ერთი ღილაკი ტექტით
- * Enter. ტექსტფილდში ტექსტის შეყვანის მერე Enter ჭილაკზე დაჭერით ტექსტი უნდა
- * იწერებოდეს(იხატებოდეს) კანვასზე, ხოლო ტექსტფილდში კი ტექსტი იშლებოდეს.
- * ხელახლა ტექსტის შეყვანისას ახალი ტექსტი წინა ტექსტის ქვემოთ უნდა გამოჩნდეს
- * (როგორც ჩატში ხდება ხოლმე). თუკი ტექსტი ჩაცდება ეკრანს არაუშავს.
+ * ისე რომ ფორმის ზომის ცვლილებაზე ბადის უჯრის ზომაც პროპორციულად იზრდებოდეს.
  */
-public class Ex70 extends GraphicsProgram {
+public class Ex70 extends GraphicsProgram implements ComponentListener {
 
+	private final int N_ROWS = 10;
+	private final int N_COLS = 10;
+
+	@Override
+	public void init() {
+		addComponentListener(this);
+	}
+
+	@Override
+	public void run() {
+		drawGrid();
+	}
+
+	private void drawGrid() {
+		// 1. Draw Horizontal Lines
+		for (int i = 0; i < N_ROWS; i++) {
+			double y = i * getHeight() / N_ROWS;
+			double x0 = 0, x1 = getWidth();
+
+			add(new GLine(x0, y, x1, y));
+		}
+
+		// 2. Draw Vertical Lines
+		for (int i = 0; i < N_COLS; i++) {
+			double x = i * getWidth() / N_COLS;
+			double y0 = 0, y1 = getHeight();
+
+			add(new GLine(x, y0, x, y1));
+		}
+	}
+
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		removeAll();
+		drawGrid();
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+	}
 }
