8bbaa4917b6dccc4276e6620306968c630ded06a
CSenshi <saba.pochkhua@gmail.com>
2021-12-31 16:22:27 +0400

    add s27

---
+++ s27/Ex71.java
@@ -0,0 +1,174 @@
+package s27;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import acm.program.ConsoleProgram;
+
+public class Ex71 extends ConsoleProgram {
+
+	/*-
+	 * School:
+	 * -	Shota:
+	 *			"Programming Methodology" - ["Luka", "Eka"]
+	 *			"Open Source System" - []
+	 *			"OOP" - ["Saba", "Ketevani"],
+	 *			"Programming Paradigms" - ["Saba", "Ketevani"]
+	 * 			
+	 * -	Nika: 
+	 *			"Discrete Math" - ["Mariami"]
+	 *			"Theory of Computation" - ["Mariami"]
+	 *			"Information Theory" - ["Mariami"]
+	 *		
+	 * -	Giorgi: 
+	 *		 	"OOP" - ["Saba", "Ketevani"]
+	 *			"Programming Paradigms" - ["Saba", "Ketevani"]
+	 *			"Distributed Systems" - ["Saba", "Ketevani"]
+	 *
+	 * -	Irakli:
+	 *			"Programming Methodology" - ["Luka", "Eka"]
+	 *			"Discrete Math" - ["Mariami"]
+	 */
+
+	@Override
+	public void run() {
+		School school = new School();
+
+		// 1. Add Teachers
+		addTeachers(school);
+
+		// 2. Add Subjects
+		addSubjects(school);
+
+		// 3. Add Pupils
+		addPupils(school);
+
+		// 4. Add More Teachers
+		addMoreTeachersAndSubjects(school);
+
+		// 5 Get pupils iterator
+		println("######### Get Pupils #########");
+		testGetPupils(school);
+
+		// 6. Get teachers iterator
+		println("######### Get Teachers #########");
+		testGetTeachers(school);
+
+		// 7. Get teachers after removing Shota
+		println("######### Get Teachers After Removing Teacher #########");
+		testRemoveTeacher(school);
+	}
+
+	private void addTeachers(School school) {
+		school.addTeacher("Shota");
+		school.addTeacher("Nika");
+		school.addTeacher("Giorgi");
+	}
+
+	private void addSubjects(School school) {
+		// Shota
+		school.addSubject("Shota", "Programming Methodology");
+		school.addSubject("Shota", "Open Source System");
+		school.addSubject("Shota", "OOP");
+		school.addSubject("Shota", "Programming Paradigms");
+		// Nika
+		school.addSubject("Nika", "Discrete Math");
+		school.addSubject("Nika", "Theory of Computation");
+		school.addSubject("Nika", "Information Theory");
+		// Giorgi
+		school.addSubject("Giorgi", "OOP");
+		school.addSubject("Giorgi", "Programming Paradigms");
+		school.addSubject("Giorgi", "Distributed Systems");
+		// Sandro (Unknown);
+		school.addSubject("Sandro", "Super Course");
+	}
+
+	private void addPupils(School school) {
+		// Saba
+		school.addPupil("Saba", "OOP");
+		school.addPupil("Saba", "OOP");
+		school.addPupil("Saba", "Programming Paradigms");
+		school.addPupil("Saba", "Distributed Systems");
+		// Ketevani
+		school.addPupil("Ketevani", "OOP");
+		school.addPupil("Ketevani", "Programming Paradigms");
+		school.addPupil("Ketevani", "Distributed Systems");
+		// Luka
+		school.addPupil("Luka", "Programming Methodology");
+		// Eka
+		school.addPupil("Eka", "Programming Methodology");
+		// Mariami
+		school.addPupil("Mariami", "Discrete Math");
+		school.addPupil("Mariami", "Theory of Computation");
+		school.addPupil("Mariami", "Information Theory");
+	}
+
+	private void addMoreTeachersAndSubjects(School school) {
+		school.addTeacher("Irakli");
+		school.addSubject("Irakli", "Programming Methodology");
+		school.addSubject("Irakli", "Discrete Math");
+	}
+
+	private void testGetPupils(School school) {
+		println("1. Shota's Pupils:");
+		Iterator<String> it1 = school.getPupils("Shota");
+		printIterator(it1);
+
+		println("\n2. Nika's Pupils:");
+		Iterator<String> it2 = school.getPupils("Nika");
+		printIterator(it2);
+
+		println("\n3. Giorgi's Pupils:");
+		Iterator<String> it3 = school.getPupils("Giorgi");
+		printIterator(it3);
+
+		println("\n4. Irakli's Pupils:");
+		Iterator<String> it4 = school.getPupils("Irakli");
+		printIterator(it4);
+
+		println();
+	}
+
+	private void testGetTeachers(School school) {
+		// 1. Saba
+		println("1. Saba's Teachers:");
+		Iterator<String> it1 = school.getTeachers("Saba");
+		printIterator(it1);
+
+		// 2. Ketevani
+		println("\n2. Ketevani's Teachers:");
+		Iterator<String> it2 = school.getTeachers("Ketevani");
+		printIterator(it2);
+
+		// 3. Luka
+		println("\n3. Luka's Teachers:");
+		Iterator<String> it3 = school.getTeachers("Luka");
+		printIterator(it3);
+
+		// 4. Eka
+		println("\n4. Eka's Teachers:");
+		Iterator<String> it4 = school.getTeachers("Eka");
+		printIterator(it4);
+
+		// 5. Eka
+		println("\n5. Mariami's Teachers:");
+		Iterator<String> it5 = school.getTeachers("Mariami");
+		printIterator(it5);
+
+		println();
+	}
+
+	private void testRemoveTeacher(School school) {
+		school.removeTeacher("Shota");
+		testGetTeachers(school);
+	}
+
+	private void printIterator(Iterator<String> it) {
+		List<String> list = new ArrayList<String>();
+		while (it.hasNext()) {
+			list.add(it.next());
+		}
+		println(list);
+	}
+}
---
+++ s27/School.java
@@ -0,0 +1,130 @@
+package s27;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class School {
+
+	private Map<String, List<String>> teachersSubjects;
+	private Map<String, List<String>> subjectsPupils;
+
+	/*
+	 * კონსტრუქტორს არაფერი არ გადაეცემა. აქ შეგიძლიათ ინიციალიზაცია გაუკეთოთ
+	 * თქვენთვის საჭირო ცვლადებს
+	 */
+	public School() {
+		teachersSubjects = new HashMap<>();
+		subjectsPupils = new HashMap<>();
+	}
+
+	/*
+	 * addTeacher მეთოდის საშუალებით შეგიძლიათ სკოლას დაამატოთ ახალი მასწავლებელი.
+	 * მეთოდს გადაეცემა მასწავლებლის სახელი. შეგიძლიათ ჩათვალოთ, რომ მასწავლებლის
+	 * სახელი უნიკალურია.
+	 */
+	public void addTeacher(String teacher) {
+		if (!teachersSubjects.containsKey(teacher)) {
+			teachersSubjects.put(teacher, new ArrayList<String>());
+		}
+	}
+
+	/*
+	 * addSubject მეთოდის საშუალებით შეგიძლიათ მასწავლებელს დაუმატოთ საგანი. მეთოდს
+	 * გადაეცემა მასწავლებლის სახელი და საგნის სახელი. ჩათვალეთ, რომ საგნის სახელი
+	 * უნიკალურია. ერთი და იგივე მასწავლებელი შეიძლება რამდენიმე საგანს კითხულობდეს,
+	 * ასევე ერთსა და იმავე საგანს შეიძლება რამდენიმე მასწავლებელი კითხულობდეს
+	 * ერთდროულად. თუკი teacher სახელის მქონე მასწავლებელი არ არის აქამდე
+	 * დამატებული, მაშინ მეთოდმა არაფერი არ უნდა გააკეთოს.
+	 */
+	public void addSubject(String teacher, String subject) {
+		if (!teachersSubjects.containsKey(teacher)) {
+			return;
+		}
+		teachersSubjects.get(teacher).add(subject);
+
+		if (!subjectsPupils.containsKey(subject)) {
+			subjectsPupils.put(subject, new ArrayList<String>());
+		}
+	}
+
+	/*
+	 * addPupil მეთოდის საშუალებით შეგიძლიათ საგანზე დაამატოთ მოსწავლე. ერთი და
+	 * იგივე მოსწავლე შეიძლება ერთ ან რამდენიმე საგანს სწავლობდეს.
+	 */
+	public void addPupil(String pupil, String subject) {
+		if (subjectsPupils.containsKey(subject)) {
+			subjectsPupils.get(subject).add(pupil);
+		}
+	}
+
+	/*
+	 * getTeachers მეთოდს გადაეცემა მოსწავლის სახელი და მან უნდა დააბრუნოს ამ
+	 * მოსწავლის ყველა მასწავლებელზე იტერატორი. ანუ მხოლოდ იმ მასწავლებლების
+	 * სახელები, რომლებიც ასწავლიან იმ საგნებს, რომლებზეც მოსწავლეა
+	 * დამატებული(სწავლობს). თუკი pupil სახელის მოსწავლე არ გვყავს მაშინ მეთოდმა
+	 * უნდა დააბრუნოს null.
+	 */
+	public Iterator<String> getTeachers(String pupil) {
+		// 1. pupil -> subjects (list)
+		List<String> subjects = new ArrayList<>();
+
+		for (String subjectName : subjectsPupils.keySet()) {
+			List<String> pupils = subjectsPupils.get(subjectName);
+			if (pupils.contains(pupil)) {
+				subjects.add(subjectName);
+			}
+		}
+
+		// 2. subjects -> teachers (list)
+		List<String> teachers = new ArrayList<>();
+		for (String teacher : teachersSubjects.keySet()) {
+			List<String> teachersSubjectsList = teachersSubjects.get(teacher);
+			if (hasIntersection(subjects, teachersSubjectsList)) {
+				teachers.add(teacher);
+			}
+		}
+		return teachers.iterator();
+	}
+
+	/*
+	 * getPupils მეთოდს გადაეცემა მასწავლებლის სახელი და მან უნდა დააბრუნოს ამ
+	 * მასწავლებლის ყველა სტუდენტზე იტერატორი. ანუ მხოლოდ იმ სტუდენტების სახელები,
+	 * რომლებიც მის რომელიმე საგანს სწავლობენ. თუკი teacher სახელის მქონე ლექტორი არ
+	 * გვყავს მაშინ მეთოდმა უნდა დააბრუნოს null.
+	 */
+	public Iterator<String> getPupils(String teacher) {
+		List<String> subjects = teachersSubjects.get(teacher);
+		List<String> pupils = new ArrayList<>();
+		for (String subjectName : subjects) {
+			List<String> subjectPupilsList = subjectsPupils.get(subjectName);
+			for (String pupil : subjectPupilsList) {
+				if (!pupils.contains(pupil)) {
+					pupils.add(pupil);
+				}
+			}
+		}
+		return pupils.iterator();
+	}
+
+	/*
+	 * მეთოდმა უნდა წაშალოს მასწავლებლის შესახებ ყველა ინფორმაცია. ამ მეთოდის
+	 * გამოძახების, შემდეგ getTeachers მეთოდმა არ უნდა დააბრუნოს teacher სახელი არც
+	 * ერთი სტუდენტისთვის.
+	 */
+	public void removeTeacher(String teacher) {
+		teachersSubjects.remove(teacher);
+	}
+
+	private boolean hasIntersection(List<String> subjects, List<String> teachersSubjectsList) {
+		for (String subject : subjects) {
+			if (teachersSubjectsList.contains(subject)) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+}
